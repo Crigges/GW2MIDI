96ee28f4f40c7ca4eb6cf78749e116739f7444c1
Crigges <andy400@web.de>
2020-08-16 15:16:09 +0200

    work with real keyboard

--- src/main/java/systems/crigges/gw2midi/GW2MIDI.java
+++ src/main/java/systems/crigges/gw2midi/GW2MIDI.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 public class GW2MIDI {
 	private MidiDevice currentDevice = null;
-	private MainFrame gui; 
+	private MainFrame gui;
 	private KeyEmulator keyEmulator;
-	
+
 	public GW2MIDI(MainFrame gui) {
 		this.gui = gui;
 		keyEmulator = new KeyEmulator(gui);
 	}
-	
+
 	public void setCurrentDevice(MidiDevice.Info newDevice) throws MidiUnavailableException {
-		if(currentDevice != null) {
+		if (currentDevice != null) {
 			currentDevice.close();
 		}
 		currentDevice = MidiSystem.getMidiDevice(newDevice);
@@ -60,21 +60,31 @@ public class GW2MIDI {
 //		}
 //	}
 
+	int[] tMap = { 0, -1, 1, -1, 2, 3, -1, 4, -1, 5, -1, 6 };
+
 	class MidiInputReceiver implements Receiver {
-		
 
 		public void send(MidiMessage msg, long timeStamp) {
 			byte[] arr = msg.getMessage();
-			//if(arr[0] >= (byte)0x90 && arr[0] <= 0x9F) {
-				gui.log("Received Key " + arr[1] + " which translates to " + (arr[1] - gui.getStartKey()));
-				keyEmulator.play(arr[1] - gui.getStartKey());
-			//}
+			StringBuilder sb = new StringBuilder();
+//			for (byte b : arr) {
+//				sb.append(String.format("%02X ", b));
+//			}
+			if (arr[0] == -112 && arr[2] != 0) {
+				int offset = (arr[1]-gui.getStartKey()) / 12 * 7;
+				System.out.println(gui.getStartKey());
+				int key = offset + tMap[(arr[1] - gui.getStartKey()) % 12];
+				gui.log("Received Key " + arr[1] + " which translates to "
+						+key);
+				keyEmulator.play(key);
+				System.out.println(offset + tMap[arr[1] % 12]);
+			}
 		}
 
 		@Override
 		public void close() {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 	}
